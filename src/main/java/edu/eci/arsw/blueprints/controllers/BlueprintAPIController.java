cdf2ba1c3cf765be597d590b86acb8abc7fbf529
StivenVanegas <nevits28@hotmail.com>
2021-02-23 19:52:29 -0500

    Parte 1 completada

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -5,6 +5,7 @@
  */
 package edu.eci.arsw.blueprints.controllers;
 
+import edu.eci.arsw.blueprints.model.Blueprint;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.logging.Level;
@@ -27,23 +28,22 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author hcadavid
  */
+@RestController
+@RequestMapping(value = "/blueprints/{author}/{bpname}")
 public class BlueprintAPIController {
-    @RestController
-    @RequestMapping(value = "/bluesSprint")
-    public class XXController {
 
-        @Autowired
-        @Qualifier("BlueprintsServices")
-        BlueprintsServices services;
-        @RequestMapping(method = RequestMethod.GET)
-        public ResponseEntity<?> manejadorGetRecursoXX() {
-            try {
-                //obtener datos que se enviarán a través del API
-                return new ResponseEntity<>(services.getAllBlueprints(), HttpStatus.ACCEPTED);
-            } catch (Exception ex) {
-                Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
-                return new ResponseEntity<>("Error bla bla bla", HttpStatus.NOT_FOUND);
-            }
+    @Autowired
+    @Qualifier("BlueprintsServices")
+    BlueprintsServices services;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> manejadorGetRecursoXX(@PathVariable String author, @PathVariable String bpname) {
+        try {
+            //obtener datos que se enviarán a través del API
+            return new ResponseEntity<>(services.getBlueprint(author,bpname), HttpStatus.ACCEPTED);
+        } catch (Exception ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error bla bla bla", HttpStatus.NOT_FOUND);
         }
     }
 }
